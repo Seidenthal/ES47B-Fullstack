6b028e10a85f16281a15d338527fd3629517cf22
Seidenthal <dseidenthal@alunos.utfpr.edu.br>
2025-05-07 13:52:25 -0300

    imports e estrutura inicial da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useReducer } from 'react';
+import { Container, Box } from '@mui/material';
+import SearchBar from './components/SearchBar';
+import GenreFilter from './components/GenreFilter';
+import MovieList from './components/MovieList';
+import FavoritesDrawer from './components/FavoritesDrawer';
+import {
+  AppContext,
+  reducer,
+  initialState,
+} from './contexts/AppReducerContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppContext.Provider value={{ state, dispatch }}>
+      <Container maxWidth="lg">
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="flex-start"
+        >
+          <Box flexGrow={1}>
+            <SearchBar />
+            <GenreFilter />
+            <MovieList />
+          </Box>
+          <FavoritesDrawer />
+        </Box>
+      </Container>
+    </AppContext.Provider>
+  );
 }
 
-export default App
+export default App;
