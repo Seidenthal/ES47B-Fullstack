8051b243a4fff2456be27af9b5eed84ba4ff4cc2
Vinicius Cerrone <vini_cerrone@hotmail.com>
2025-07-05 20:26:31 -0300

    Possivel Solução GitHub pages

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter basename="/ES47B-Fullstack/">
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/">
+          <Route index element={<App />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
